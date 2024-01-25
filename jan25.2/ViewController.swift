502ff3a8f121b75f37e8fcfdb620d312536226e6
Sam O <2274654+samogden@users.noreply.github.com>
2024-01-25 14:17:35 -0800

    Initial Commit

---
+++ jan25.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jan25.2
+//
+//  Created by Sam Ogden on 1/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
